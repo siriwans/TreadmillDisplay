2da18bd512819d509e296cb629b8127b74b9eadf
jaelyndomingo <jdomin31@calpoly.edu>
2019-05-14 11:37:24 -0700

    display

--- Display.py
+++ Display.py
@@ -40,7 +40,68 @@ class Display(Frame):
 
         canvas.pack(fill = BOTH, expand = 1)
 
+def anotherWindow():
 
+    window2 = Tk()
+    window2.wm_attributes('-fullscreen', 'true')
+
+    width = window2.winfo_screenwidth()
+    height = window2.winfo_screenheight()
+    window2.geometry('%dx%d' % (width, height))
+
+    # window['bg'] = bgColor
+
+    canvas = Canvas(window2, background="#4fa4ef")
+    canvas.pack(fill=BOTH, expand=1)
+
+    # Top Buttons (images)
+
+    pil_plane = PIL.Image.open("buttonL.gif")
+    width_og, height_og = pil_plane.size
+    factor = 1
+    widthi = int(width_og * factor)
+    heighti = int(height_og * factor)
+    pil_plane1 = pil_plane.resize((widthi, heighti), PIL.Image.ANTIALIAS)
+    gif1 = PIL.ImageTk.PhotoImage(pil_plane1)
+    # canvas.create_image((width / 2) - 150, 0, image=gif1, anchor=NE)
+    planeButton = Button(width=widthi, height=heighti, bd=0, highlightthickness=0, bg="#0b5394", relief=FLAT,
+                         image=gif1, activebackground="#0b5394",
+                         activeforeground="#0b5394", highlightcolor="#4fa4ef", highlightbackground="#4fa4ef")
+
+    planeButton.pack()
+    planeButton.place(x=(width / 2) - 150, y=0, anchor=NE)
+
+    pil_run = PIL.Image.open("buttonM.gif")
+    width_og, height_og = pil_run.size
+    factor = 1
+    widthi = int(width_og * factor)
+    heighti = int(height_og * factor)
+    pil_run1 = pil_run.resize((widthi, heighti), PIL.Image.ANTIALIAS)
+    gif2 = PIL.ImageTk.PhotoImage(pil_run1)
+    # canvas.create_image((width / 2), 0, image=gif2, anchor=N)
+    runButton = Button(width=widthi, height=heighti, bd=0, highlightthickness=0, bg="#0b5394", relief=FLAT,
+                       image=gif2, activebackground="#0b5394",
+                       activeforeground="#0b5394", highlightcolor="#4fa4ef", highlightbackground="#4fa4ef")
+
+    runButton.pack()
+    runButton.place(x=(width / 2), y=0, anchor=N)
+
+    pil_film = PIL.Image.open("buttonR.gif")
+    width_og, height_og = pil_film.size
+    factor = 1
+    widthi = int(width_og * factor)
+    heighti = int(height_og * factor)
+    pil_film1 = pil_film.resize((widthi, heighti), PIL.Image.ANTIALIAS)
+    gif3 = PIL.ImageTk.PhotoImage(pil_film1)
+    # canvas.create_image((width / 2) + 150, 0, image=gif3, anchor=NW)
+    filmButton = Button(width=widthi, height=heighti, bd=0, highlightthickness=0, bg="#0b5394", relief=FLAT,
+                        image=gif3, activebackground="#0b5394",
+                        activeforeground="#0b5394", highlightcolor="#4fa4ef", highlightbackground="#4fa4ef")
+
+    filmButton.pack()
+    filmButton.place(x=(width / 2) + 150, y=0, anchor=NW)
+    window2.mainloop()
+    return
 
 def main():
     bgColor = "#4fa4ef"
@@ -69,7 +130,7 @@ def main():
     # canvas.create_image((width / 2) - 150, 0, image=gif1, anchor=NE)
     planeButton = Button(width=widthi, height=heighti, bd=0, highlightthickness=0, bg="#0b5394", relief=FLAT,
                          image=gif1, activebackground="#0b5394",
-                         activeforeground="#0b5394", highlightcolor="#4fa4ef", highlightbackground="#4fa4ef")
+                         activeforeground="#0b5394", highlightcolor="#4fa4ef", highlightbackground="#4fa4ef", command=anotherWindow)
 
     planeButton.pack()
     planeButton.place(x=(width / 2) - 150, y=0, anchor=NE)
